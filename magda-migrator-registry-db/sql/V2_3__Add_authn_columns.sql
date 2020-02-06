e7ae599d2cb15b1171a951a04b5ab61ea01e4e99
Alex Gilleran <alex.gilleran@nicta.com.au>
2020-02-06 14:39:47 +1100

    Added index to authnreadpolicy

--- magda-migrator-registry-db/sql/V2_3__Add_authn_columns.sql
+++ magda-migrator-registry-db/sql/V2_3__Add_authn_columns.sql
@@ -1,2 +1,6 @@
 
 ALTER TABLE records ADD COLUMN authnReadPolicyId VARCHAR;
+CREATE INDEX records_authnreadpolicyid_idx
+    ON test.records USING btree
+    (authnreadpolicyid ASC NULLS LAST)
+    TABLESPACE pg_default;
